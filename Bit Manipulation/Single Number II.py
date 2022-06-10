b324c3c0432d6bc50a7364485b9f6756b37ba403
Ani0202 <agrawalaniket02@gmail.com>
2022-06-10 17:41:55 +0530

    Solutions

---
+++ Bit Manipulation/Single Number II.py
@@ -0,0 +1,37 @@
+"""
+Given an array of integers, every element appears thrice except for one which occurs once.
+
+Find that element which does not appear thrice.
+
+Note: Your algorithm should have a linear runtime complexity.
+
+Could you implement it without using extra memory?
+
+Input Format:
+
+    First and only argument of input contains an integer array A
+Output Format:
+
+    return a single integer.
+Constraints:
+
+2 <= N <= 5 000 000
+0 <= A[i] <= INT_MAX
+"""
+
+
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def singleNumber(self, A):
+        ones = 0
+        twos = 0
+
+        for elem in A:
+            ones = (ones ^ elem) & ~twos
+            twos = (twos ^ elem) & ~ones
+
+        return ones
+
+
+
